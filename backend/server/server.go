dc611825712b2914bc11dbcf5151ca521c055873
Jerome-afk <otienojerome7@gmail.com>
2024-12-14 02:26:08 +0300

    connecting to the db

--- backend/server/server.go
+++ backend/server/server.go
@@ -1,11 +1,46 @@
 package server
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
+func checkConnection() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	//Prepare the database
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUsername, dbPassword, dbHost, dbPort, dbName)
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal("Error opening database: ", err)
+	}
+	defer db.Close()
+
+	// Ping the database
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Error connecting to the database: ", err)
+	}
+	fmt.Println("Database connection successful")
+}
+
 func Start() *gin.Engine {
+	checkConnection()
 	router := gin.Default()
 
 	router.GET("/api/data", func(ctx *gin.Context) {
